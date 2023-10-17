c26811ac953d66595bbd5e2dea76c029db895709
Heitor Gonçalves <contato.heitor@icloud.com>
2023-10-16 22:36:58 -0300

    first commit

---
+++ src/test/java/me/dio/mediojavabackend/MeDioJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.mediojavabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeDioJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
